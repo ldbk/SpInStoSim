32cea5629d4626eb1fa99ff6608449c1b6dddd5f
morganeamelot <morgane.amelot@ifremer.fr>
2020-11-12 15:04:36 +0100

    n'est pas vast qui veut

--- analyses/from_geo_to_abind/vast_apply.R
+++ analyses/from_geo_to_abind/vast_apply.R
@@ -38,41 +38,47 @@ for ( j in 1:25){ # iteration
                               long=rep(Res.red[[stock_name]][[iteration]][["sample.expo"]][["x2"]],n_ts),
                               year=rep(1:n_ts, each=90), survey=rep(NA,90*n_ts),s_a=rep(1,90*n_ts))
   lindex_min<-1
-  lindex_max<-60  
-for (l in 1:60){ #year 
+  lindex_max<-90  
+for (l in 1:90){ #year 
   simul<-paste(c("Simu.V1.S",l), collapse="")
   vast_format[[i]][[j]][["example_sphe"]][["survey"]][lindex_min:lindex_max]<-Res.red[[stock_name]][[iteration]][["sample.sphe"]][[simul]]
   vast_format[[i]][[j]][["example_gaus"]][["survey"]][lindex_min:lindex_max]<-Res.red[[stock_name]][[iteration]][["sample.gaus"]][[simul]]
   vast_format[[i]][[j]][["example_cub"]][["survey"]][lindex_min:lindex_max]<-Res.red[[stock_name]][[iteration]][["sample.cub"]][[simul]]
   vast_format[[i]][[j]][["example_expo"]][["survey"]][lindex_min:lindex_max]<-Res.red[[stock_name]][[iteration]][["sample.expo"]][[simul]]
 lindex_min<-lindex_max+1
-lindex<-lindex_max+n_ts
+lindex<-lindex_max+90
 
 } # year
 } #iteration
 } #stock 
 
-
+t<-seq(0.005,0.995, length.out=100)
+tt<-expand.grid(t,t)
+times<-c(400,1200,400,1200,3600,1200,400,1200,400)/3
+r <- rep(c(1:9), times=c(400,1200,400,1200,3600,1200,400,1200,400))
 
 example<-list(vast_format[[1]][[1]][["example_sphe"]],vast_format[[1]][[1]][["example_gaus"]])
 indices<-list(NA,NA)
+imax<-2
 foreach(i = 1:2) %dopar%
 library(FishStatsUtils)
 library(VAST)
 library(doParallel)
-for(i in 1:1){
+for(i in 1:imax){
   # Make settings (turning off bias.correct to save time for example)
-  settings = make_settings( n_x=90, 
+  settings = make_settings( n_x=100, 
                             Region="user", 
                             purpose="index2", 
                             strata.limits= data.frame(STRATA = "All_areas"), 
                             bias.correct=FALSE )
   
   m_ll<-matrix(data=NA, nrow=90, ncol=3)
+ # m_ll<-matrix(data=NA, nrow=90, ncol=2)
   m_ll[,1]<-example[[i]][["lat"]][1:90]
-  m_ll[,2]<-example[[i]][["long"]][1:90]
-  m_ll[,3]<-rep(1,90)
+  m_ll[,2]<-example[[i]][["lat"]][1:90]
+  m_ll[,3]<-rep(c(400,1200,400,1200,3600,1200,400,1200,400)/3, each=10)
   colnames(m_ll)<-c('Lat', 'Lon','Area_km2')
+  #colnames(m_ll)<-c('Lat', 'Lon')
   
 
   # Run model
@@ -86,7 +92,9 @@ for(i in 1:1){
                    #,observations_LL =m_ll
                    ,input_grid=m_ll
                    #v_i=example[[i]]$sampling_data[,'Vessel'] 
-                   )
+                   ,Aniso=FALSE, 
+                   FieldConfig= c("Omega1"=0, "Epsilon1"=0, "Omega2"=1, "Epsilon2"=1))
+                 
   
   indices[[i]]<-fit$Report$Index_ctl[1,,1]
 }
